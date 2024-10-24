5c1cbebab4944dc94614af1c9529bf69f82a63df
MagicianJoker <1159446117@qq.com>
2024-10-24 19:40:44 +0800

    add

---
+++ web-mobile/src/settings.js
@@ -0,0 +1,40 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "block",
+        "rock",
+        "tool",
+        "Ui"
+    ],
+    collisionMatrix: [
+        [
+            false,
+            true,
+            true
+        ],
+        [
+            true,
+            false,
+            false
+        ],
+        [
+            true,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/loading.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: []
+};
